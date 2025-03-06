b778834761be2a4aeb887504b41f9421c5d4eb1b
protocorn <chordiasahil24@gmail.com>
2025-03-06 10:33:31 -0500

    access token

--- node_server/config/apiKeys.js
+++ node_server/config/apiKeys.js
@@ -1,6 +1,6 @@
 module.exports = {
     GITHUB_API_URL: "https://api.github.com/",
-    GITHUB_TOKEN: "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", // Replace with actual token
+    GITHUB_TOKEN: "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", // Replace with actual token
     USER_AGENT: "protocorn",
     REDDIT_CLIENT_ID: "sDEzkNnfLcmnoaKlxxteHA",
     REDDIT_CLIENT_SECRET: "7wIB4OIg5VyKd_JRJMGwCJ5qn-KQXQ",
